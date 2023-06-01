263fd719c63d13c5d7318f21beb0d6d0f407b38f
ThiagoROX <thiagokenis@live.com>
2023-06-01 03:05:07 -0300

    Melhoria da sugestão de subcomandos

--- duels-plugin/src/main/java/me/realized/duels/command/commands/duel/subcommands/DenyCommand.java
+++ duels-plugin/src/main/java/me/realized/duels/command/commands/duel/subcommands/DenyCommand.java
@@ -11,7 +11,7 @@ import org.bukkit.entity.Player;
 public class DenyCommand extends BaseCommand {
 
     public DenyCommand(final DuelsPlugin plugin) {
-        super(plugin, "deny", "deny [player]", "Declines a duel request.", 2, true);
+        super(plugin, "negar", "deny [player]", "Declines a duel request.", 2, true, "deny");
     }
 
     @Override
