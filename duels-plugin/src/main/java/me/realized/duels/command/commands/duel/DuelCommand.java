82626efada955e2d3b42cebe3a3d4b60c467a3c9
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-07-31 06:52:06 -0300

    Resolvido problema com sugestão de comandos

--- duels-plugin/src/main/java/me/realized/duels/command/commands/duel/DuelCommand.java
+++ duels-plugin/src/main/java/me/realized/duels/command/commands/duel/DuelCommand.java
@@ -277,7 +277,7 @@ public class DuelCommand extends BaseCommand {
             List<String> completions = new ArrayList<>();
 
             Iterable<String> players = Bukkit.getOnlinePlayers().stream()
-                    .filter(p -> !player.canSee(p))
+                    .filter(player::canSee)
                     .filter(p -> p != player)
                     .map(Player::getName)
                     .toList();
